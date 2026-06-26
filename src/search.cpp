14e44e95894e17b4f99d08cad51a8c43865b54a6
GoobusTheNoobus <huand2464@wrdsb.ca>
2026-06-26 16:28:12 -0400

    organized serach

--- src/search.cpp
+++ src/search.cpp
@@ -47,38 +47,7 @@ namespace Crystall::Search {
         }
     };
 
-    struct SearchInfo {
-        u64 nodes_searched = 0;
-        u64 previous_nodes_searched = 0;
-        int plies_from_root = 0;
-        int seldepth = 0;
-    };
-
-    struct RootSearchResult {
-        int score = 0;
-        Move move;
-    };
-
-    int perft(Position& pos, int depth_left) {
-
-        if (depth_left <= 0) return 1;
-
-        int nodes = 0;
-
-        MoveList moves(pos);
-        for (int i = 0; i < moves.size(); ++i) {
-            Move move = moves[i];
-
-            bool is_legal = pos.attempt_move(move);
-            if (!is_legal) continue;
-
-            nodes += perft(pos, depth_left - 1);
-
-            pos.undo_move();
-        }
-
-        return nodes;
-    }
+    // helper functions here
 
     bool is_noisy(Position& pos, Move move) {
         Move::Type flag = move.flag();
@@ -86,6 +55,121 @@ namespace Crystall::Search {
         if (flag >= Move::PromoQ || flag == Move::EnPassant) return true;
 
         return pos.get_piece_on(move.dest()) != NoPiece;
+    }    
+
+    // main functions
+    void start_search(Position& pos, int max_depth, int movetime) {
+        int score = 0;
+
+        SearchTimer::start(movetime);
+
+        SearchInfo info;
+
+        Move best_move;
+
+        UCI::info_string("score cp " + std::to_string(pos.evaluate()));
+
+        for (int depth = 1; depth <= max_depth; ++depth) {
+            info.previous_nodes_searched = info.nodes_searched;
+            info.seldepth = 0;
+
+            RootSearchResult result = search_root(info, pos, depth);
+
+            if (SearchTimer::timeout_search()) break;
+
+            best_move = result.move;
+
+            UCI::info_depth(depth, info.seldepth, result.score, info.nodes_searched - info.previous_nodes_searched, SearchTimer::elapsed_ms(), info.nodes_searched, result.move);
+        }
+
+        std::cout << "bestmove " << best_move.to_string() << std::endl;
+    }
+
+    void stop_search() {
+        SearchTimer::stop_flag = true;
+    }
+
+    RootSearchResult search_root(SearchInfo& info, Position& pos, int depth) {
+        MoveList moves(pos);
+        int best_score = -Infinity;
+        Move best_move;
+
+        for (int i = 0; i < moves.size(); ++i) {
+
+            Move move = moves[i];
+
+            bool is_legal = pos.attempt_move(move);
+            if (!is_legal) continue;
+
+            int score = -search_node(info, pos, depth - 1, -Infinity, Infinity);
+
+            pos.undo_move();
+
+            if (score > best_score) {
+                best_score = score;
+                best_move = move;
+            }
+
+            if (SearchTimer::timeout_search()) break;
+        }
+
+        return {best_score, best_move};
+    }
+
+    int search_node(SearchInfo& info, Position& pos, int depth, int alpha, int beta) {
+        info.nodes_searched++;
+
+        if (SearchTimer::timeout_search())
+            return Timeout;
+
+        info.plies_from_root++;
+        info.seldepth = std::max(info.seldepth, info.plies_from_root);
+
+        if (pos.is_repetition() || pos.is_rule_50()) {
+            info.plies_from_root--;
+            return DrawScore;
+        }
+
+        if (depth <= 0) {
+            
+            int score = qsearch_node(info, pos, SearchOptions::MaxQSearchDepth, alpha, beta);
+            info.plies_from_root--;
+            return score;
+        }
+
+        MoveList moves(pos);
+        int best_score = -Infinity;
+        bool has_legal_move = false;
+
+        for (int i = 0; i < moves.size(); ++i) {
+            Move move = moves[i];
+
+            bool is_legal = pos.attempt_move(move);
+            if (!is_legal) continue;
+
+            has_legal_move = true;
+
+            int score = -search_node(info, pos, depth - 1, -beta, -alpha);
+
+            pos.undo_move();
+
+            best_score = std::max(best_score, score);
+            alpha = std::max(alpha, score);
+
+            if (alpha >= beta)
+                break;
+        }
+
+        int result;
+        if (!has_legal_move) {
+            Color us = pos.get_side_to_move();
+            result = pos.is_in_check(us) ? -(MateScore - info.plies_from_root) : DrawScore;
+        } else {
+            result = best_score;
+        }
+
+        info.plies_from_root--;
+        return result;
     }
 
     int qsearch_node(SearchInfo& info, Position& pos, int depth, int alpha, int beta) {
@@ -165,95 +249,6 @@ namespace Crystall::Search {
         return alpha;
     }
 
-    int search_node(SearchInfo& info, Position& pos, int depth, int alpha, int beta) {
-        info.nodes_searched++;
-
-        if (SearchTimer::timeout_search())
-            return Timeout;
-
-        info.plies_from_root++;
-        info.seldepth = std::max(info.seldepth, info.plies_from_root);
-
-        if (pos.is_repetition() || pos.is_rule_50()) {
-            info.plies_from_root--;
-            return DrawScore;
-        }
-
-        if (depth <= 0) {
-            
-            int score = qsearch_node(info, pos, SearchOptions::MaxQSearchDepth, alpha, beta);
-            info.plies_from_root--;
-            return score;
-        }
-
-        MoveList moves(pos);
-        int best_score = -Infinity;
-        bool has_legal_move = false;
-
-        for (int i = 0; i < moves.size(); ++i) {
-            Move move = moves[i];
-
-            bool is_legal = pos.attempt_move(move);
-            if (!is_legal) continue;
-
-            has_legal_move = true;
-
-            int score = -search_node(info, pos, depth - 1, -beta, -alpha);
-
-            pos.undo_move();
-
-            best_score = std::max(best_score, score);
-            alpha = std::max(alpha, score);
-
-            if (alpha >= beta)
-                break;
-        }
-
-        int result;
-        if (!has_legal_move) {
-            Color us = pos.get_side_to_move();
-            result = pos.is_in_check(us) ? -(MateScore - info.plies_from_root) : DrawScore;
-        } else {
-            result = best_score;
-        }
-
-        info.plies_from_root--;
-        return result;
-    }
-
-    RootSearchResult search_root(SearchInfo& info, Position& pos, int depth) {
-        MoveList moves(pos);
-        int best_score = -Infinity;
-        Move best_move;
-
-        for (int i = 0; i < moves.size(); ++i) {
-
-            Move move = moves[i];
-
-            bool is_legal = pos.attempt_move(move);
-            if (!is_legal) continue;
-
-            if (info.plies_from_root != 0) {
-                std::cerr << "Plies from root is non zero at root\n";
-                std::cerr << "Plies from root: " << info.plies_from_root << std::endl;
-                std::exit(-1);
-            }
-
-            int score = -search_node(info, pos, depth - 1, -Infinity, Infinity);
-
-            pos.undo_move();
-
-            if (score > best_score) {
-                best_score = score;
-                best_move = move;
-            }
-
-            if (SearchTimer::timeout_search()) break;
-        }
-
-        return {best_score, best_move};
-    }
-
     void perft_divide(Position& pos, int depth) {
         int total_nodes = 0;
 
@@ -282,34 +277,24 @@ namespace Crystall::Search {
 
     }
 
-    void start_search(Position& pos, int max_depth, int movetime) {
-        int score = 0;
+    int perft(Position& pos, int depth_left) {
 
-        SearchTimer::start(movetime);
+        if (depth_left <= 0) return 1;
 
-        SearchInfo info;
+        int nodes = 0;
 
-        Move best_move;
+        MoveList moves(pos);
+        for (int i = 0; i < moves.size(); ++i) {
+            Move move = moves[i];
 
-        UCI::info_string("score cp " + std::to_string(pos.evaluate()));
+            bool is_legal = pos.attempt_move(move);
+            if (!is_legal) continue;
 
-        for (int depth = 1; depth <= max_depth; ++depth) {
-            info.previous_nodes_searched = info.nodes_searched;
-            info.seldepth = 0;
+            nodes += perft(pos, depth_left - 1);
 
-            RootSearchResult result = search_root(info, pos, depth);
-
-            if (SearchTimer::timeout_search()) break;
-
-            best_move = result.move;
-
-            UCI::info_depth(depth, info.seldepth, result.score, info.nodes_searched - info.previous_nodes_searched, SearchTimer::elapsed_ms(), info.nodes_searched, result.move);
+            pos.undo_move();
         }
 
-        std::cout << "bestmove " << best_move.to_string() << std::endl;
-    }
-
-    void stop_search() {
-        SearchTimer::stop_flag = true;
+        return nodes;
     }
 }
--- src/search.hpp
+++ src/search.hpp
@@ -6,8 +6,24 @@
 namespace Crystall::Search {
     constexpr int MaxSearchDepth = 100;
 
+    struct SearchInfo {
+        u64 nodes_searched = 0;
+        u64 previous_nodes_searched = 0;
+        int plies_from_root = 0;
+        int seldepth = 0;
+    };
+
+    struct RootSearchResult {
+        int score = 0;
+        Move move;
+    };
+
     void start_search(Position& pos, int depth, int movetime);
     void stop_search();
+
+    int qsearch_node(SearchInfo& info, Position& pos, int depth, int alpha, int beta);
+    int search_node(SearchInfo& info, Position& pos, int depth, int alpha, int beta);
+    RootSearchResult search_root(SearchInfo& info, Position& pos, int depth);
 
     void perft_divide(Position& pos, int depth);
     int perft(Position& pos, int depth_left);
